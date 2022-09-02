e26478bac2df422eb5e7d06b9572bf3043fcd9b5
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2022-09-02 18:13:30 +0300

    9-bff: Update environment mapping, add example

--- bff-service/index.js
+++ bff-service/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 app.all('/*', (req, res) => {
-  console.log('\noriginalUrl', req.originalUrl); // products/main?res=all
+  console.log('\noriginalUrl', req.originalUrl); // products?res=all
   console.log('method', req.method); // POST, GET
   console.log('body', req.body); // { name: 'product-1', count: 12 }
 
@@ -20,7 +20,32 @@ app.all('/*', (req, res) => {
   const recipientUrl = process.env[recipient];
   console.log('recipientUrl', recipientUrl);
 
-  res.status(200).json(req.body || {});
+  if (!recipientUrl) {
+    return res.status(502).json({ error: 'Cannot process request' });
+  }
+
+  const axiosConfig = {
+    method: req.method,
+    url: `${recipientUrl}${req.originalUrl}`,
+    ...(Object.keys(req.body || {}).length > 0 && { data: req.body }),
+  };
+
+  console.log('axiosConfig', axiosConfig);
+
+  axios(axiosConfig)
+    .then(({ data }) => {
+      console.log('response from recipient', data);
+      return res.json(data);
+    })
+    .catch((error) => {
+      console.log('some error', JSON.stringify(error));
+
+      if (error.response) {
+        const { status, data } = error.response;
+        return res.status(status).json(data);
+      }
+      return res.status(500).json({ error: error.message });
+    });
 });
 
 app.listen(PORT, () => {
